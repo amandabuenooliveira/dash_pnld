982d41d48771bf48bbfa958635f392e2f8dc2c43
Amanda <amanda.bueno.oliveira@outlook.com>
2024-09-11 10:45:14 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('PNLD - Ensino Médio')
+
+st.write("Escolas do Brasil")
+
+st.metric(label="Alunado EM", value="6.690.396")
+
+st.metric(label="Professores EM", value="4508.317")
+
+st.metric(label="Escolas EM", value="21.016")
+
+st.metric(label="Municípios com EM", value="5.570")
+
+st.metric(label="Estados com EM", value="27")
+
+st.sidebar.write("Filtros")
+st.sidebar.write("Escolha o estado")
+estado = st.sidebar.selectbox('Estado', ['SP', 'RJ', 'MG', 'RS', 'PR'])
+
